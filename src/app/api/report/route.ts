c0e1c5fea190336a0ad9bd484ae74f4349a41e73
Quang Trinh <khacquang.trinh@gmail.com>
2024-11-10 16:41:13 +0700

    Revert "Try intercepting requests to serve static files"
    This reverts commit 8f1eed68a9259543a0fdb557fbe9c7af6a730259.

--- src/app/api/report/route.ts
+++ src/app/api/report/route.ts
@@ -2,7 +2,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { NextRequest, NextResponse } from "next/server";
 import chromium from "@sparticuz/chromium";
-import fs from "fs";
 import fsPromise from "fs/promises";
 import path from "path";
 import { PDFDocument } from "pdf-lib";
@@ -16,29 +15,6 @@ const puppeteer: any = isProduction
   : await import("puppeteer");
 let browser: Browser | null = null;
 await getBrowserInstance();
-
-const staticDir = path.join(process.cwd(), "public/static");
-const staticURLPath = "/static";
-
-const mimeTypes: Record<string, string> = {
-  html: "text/html",
-  js: "text/javascript",
-  css: "text/css",
-  png: "image/png",
-  jpg: "image/jpg",
-  jpeg: "image/jpeg",
-  gif: "image/gif",
-  svg: "image/svg+xml",
-  ttf: "font/ttf",
-  otf: "font/otf",
-  woff: "font/woff",
-  woff2: "font/woff2",
-};
-
-function getContentTypeFromPath(filePath: string): string | undefined {
-  const extname = path.extname(filePath).slice(1);
-  return mimeTypes[extname];
-}
 
 // Launch or reuse the browser instance
 async function getBrowserInstance(): Promise<Browser> {
@@ -54,6 +30,12 @@ async function getBrowserInstance(): Promise<Browser> {
         "--disable-gpu",
         "--hide-scrollbars",
         "--disable-web-security",
+        "--disable-extensions",
+        "--disable-infobars",
+        "--disable-notifications",
+        "--no-first-run",
+        "--disable-background-networking",
+        "--disable-background-timer-throttling",
       ],
       headless: true,
       ...(isProduction
@@ -62,7 +44,10 @@ async function getBrowserInstance(): Promise<Browser> {
           }
         : {}),
     });
+    return browser!;
   }
+
+  console.log("Reusing existing browser instance...");
   return browser!;
 }
 
@@ -100,81 +85,13 @@ async function renderPagesToMergedPDF(
     const additionalPageUrl = `${process.env.NEXT_PUBLIC_BASE_URL}/other-page`;
 
     const browser = await getBrowserInstance();
-    const page = await browser.newPage();
-
-    // Intercept requests to serve static files
-    await page.setRequestInterception(true);
-    page.on("request", (request) => {
-      if (request.isInterceptResolutionHandled()) {
-        return;
-      }
-
-      const requestUrl = new URL(request.url());
-      const requestPath = requestUrl.pathname;
-      const requestOrigin = requestUrl.origin;
-
-      if (
-        requestOrigin === process.env.NEXT_PUBLIC_BASE_URL &&
-        (requestPath.startsWith(staticURLPath) ||
-          requestPath.startsWith("/_next/static"))
-      ) {
-        let filePath;
-        if (requestPath.startsWith("/_next/static")) {
-          // Handle Next.js static assets
-          const nextStaticDir = path.join(process.cwd(), ".next/static");
-          filePath = path.join(
-            nextStaticDir,
-            requestPath.replace("/_next/static/", "")
-          );
-        } else {
-          // Handle custom static assets in the public/static directory
-          filePath = path.join(
-            staticDir,
-            requestPath.slice(staticURLPath.length)
-          );
-        }
-
-        console.log(`Attempting to serve file from: ${filePath}`);
-
-        const contentType = getContentTypeFromPath(filePath);
-        let fileContent;
-
-        if (contentType) {
-          try {
-            fileContent = fs.readFileSync(filePath);
-            console.log(`Intercepted request for ${filePath}: Success`);
-          } catch {
-            console.error(`Failed to read file at ${filePath}`);
-          }
-        }
-
-        if (fileContent) {
-          request.respond({
-            status: 200,
-            contentType,
-            headers: {
-              "Cache-Control": "max-age=600, stale-while-revalidate=300",
-            },
-            body: fileContent,
-          });
-        } else {
-          console.error(`File not found: ${filePath}`);
-          request.respond({
-            status: 404,
-            contentType: "text/html",
-            body: "File not found",
-          });
-        }
-      } else {
-        console.log(`Request passed through: ${request.url()}`);
-        request.continue();
-      }
-    });
+    const page = await browser!.newPage();
 
     try {
       await page.goto(mainUrl, { waitUntil: "networkidle0" });
 
       if (!isProduction) {
+        // Take a screenshot for debugging in development
         await page.screenshot({
           path: mainPdfPath.replace(".pdf", ".png"),
           fullPage: true,
@@ -200,6 +117,7 @@ async function renderPagesToMergedPDF(
       await page.waitForNavigation({ waitUntil: "networkidle0" });
 
       if (!isProduction) {
+        // Take a screenshot for debugging in development
         await page.screenshot({
           path: additionalPdfPath.replace(".pdf", ".png"),
           fullPage: true,
@@ -211,10 +129,18 @@ async function renderPagesToMergedPDF(
         format: "A4",
         path: additionalPdfPath,
         printBackground: true,
-        margin: { top: "0in", right: "0in", bottom: "0in", left: "0in" },
+        margin: {
+          top: "0in",
+          right: "0in",
+          bottom: "0in",
+          left: "0in",
+        },
       });
     } catch (error) {
-      console.error(`Error rendering pages to PDF:`, error);
+      console.error(
+        `Error rendering page at ${additionalPageUrl} to PDF:`,
+        error
+      );
     } finally {
       await page.close();
     }
